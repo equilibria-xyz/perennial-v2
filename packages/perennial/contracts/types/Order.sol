27a5b41a8ee960194a19b00e10953e31ad6263f9
Kevin Britz <kevin@equilibria.xyz>
2023-07-01 19:10:34 -0700

    only charge keeper fee when request happens

--- packages/perennial/contracts/types/Order.sol
+++ packages/perennial/contracts/types/Order.sol
@@ -40,7 +40,7 @@ library OrderLib {
         self.fee = self.maker.abs().mul(latestVersion.price.abs()).mul(UFixed6Lib.from(makerFee))
             .add(self.long.abs().add(self.short.abs()).mul(latestVersion.price.abs()).mul(UFixed6Lib.from(takerFee)));
 
-        self.keeper = protocolParameter.settlementFee;
+        self.keeper = isEmpty(self) ? UFixedLib.ZERO : protocolParameter.settlementFee;
     }
 
     function increasesPosition(Order memory self) internal pure returns (bool) {
