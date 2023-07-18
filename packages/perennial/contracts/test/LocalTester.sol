74832bfdf1eb08d9a05bf89ba5092a3a7c01b633
Arjun Rao <2940142+arjun-io@users.noreply.github.com>
2023-07-17 20:37:12 -0600

    Add 'Local' lib tests

---
+++ packages/perennial/contracts/test/LocalTester.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.13;
+
+import "../types/Local.sol";
+
+contract LocalTester {
+    LocalStorage public local;
+
+    function read() external view returns (Local memory) {
+        return local.read();
+    }
+
+    function store(Local memory newLocal) external {
+        return local.store(newLocal);
+    }
+
+    function update(Fixed6 collateral) external {
+        Local memory newLocal = local.read();
+        newLocal.update(collateral);
+        local.store(newLocal);
+    }
+
+    function accumulate(
+        Position memory fromPosition,
+        Position memory toPosition,
+        Version memory fromVersion,
+        Version memory toVersion
+    ) external returns (LocalAccumulationResult memory values) {
+        Local memory newLocal = local.read();
+        values = newLocal.accumulate(fromPosition, toPosition, fromVersion, toVersion);
+        local.store(newLocal);
+    }
+
+    function protect(
+        Position memory latestPosition,
+        uint256 currentTimestamp,
+        bool tryProtect
+    ) external returns (bool protection) {
+        Local memory newLocal = local.read();
+        protection = newLocal.protect(latestPosition, currentTimestamp, tryProtect);
+        local.store(newLocal);
+    }
+
+    function clearReward() external {
+        Local memory newLocal = local.read();
+        newLocal.clearReward();
+        local.store(newLocal);
+    }
+}
