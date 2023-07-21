e825e66eba56c030740b344f5d01b77fa650957c
Arjun Rao <2940142+arjun-io@users.noreply.github.com>
2023-07-21 10:49:36 -0400

    Add 'VaultParameter' lib tests

---
+++ packages/perennial-vault/contracts/test/VaultParameterTest.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.13;
+
+import "../types/VaultParameter.sol";
+
+contract VaultParameterTester {
+    VaultParameterStorage public vaultParameter;
+
+    function store(VaultParameter memory newVaultParameter) external {
+        vaultParameter.store(newVaultParameter);
+    }
+
+    function read() external view returns (VaultParameter memory) {
+        return vaultParameter.read();
+    }
+}
