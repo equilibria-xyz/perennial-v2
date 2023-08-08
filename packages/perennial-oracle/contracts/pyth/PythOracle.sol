35f0554813cc87278b2ccccf362a8b70007e3a60
Arjun Rao <2940142+arjun-io@users.noreply.github.com>
2023-08-08 18:31:24 -0400

    Add oracle request/fulfilled events

--- packages/perennial-oracle/contracts/pyth/PythOracle.sol
+++ packages/perennial-oracle/contracts/pyth/PythOracle.sol
@@ -78,10 +78,12 @@ contract PythOracle is IPythOracle, Instance, Kept {
 
     /// @notice Records a request for a new oracle version
     /// @dev Original sender to optionally use for callbacks
-    function request(address) external onlyAuthorized {
+    function request(address sender) external onlyAuthorized {
         uint256 currentTimestamp = current();
-        if (versionList.length == 0 || versionList[versionList.length - 1] != currentTimestamp)
+        if (versionList.length == 0 || versionList[versionList.length - 1] != currentTimestamp) {
             versionList.push(currentTimestamp);
+            emit OracleProviderVersionRequested(sender, currentTimestamp);
+        }
     }
 
     /// @notice Returns the latest synced oracle version and the current oracle version
@@ -158,6 +160,8 @@ contract PythOracle is IPythOracle, Instance, Kept {
         _recordPrice(versionToCommit, pythPrice);
         nextVersionIndexToCommit = versionIndex + 1;
         _latestVersion = versionToCommit;
+
+        emit OracleProviderVersionFulfilled(versionToCommit);
     }
 
     /// @notice Commits the price to a non-requested version
