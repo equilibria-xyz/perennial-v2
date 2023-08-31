92711d59aa5d98fd793c6017961ef9d7cd4d15ab
nd <79500129+test9955667@users.noreply.github.com>
2023-08-31 15:12:53 -0400

    factory and oracle errors

--- packages/perennial/contracts/interfaces/IMarketFactory.sol
+++ packages/perennial/contracts/interfaces/IMarketFactory.sol
@@ -10,10 +10,14 @@ interface IMarketFactory is IFactory {
     event OperatorUpdated(address indexed account, address indexed operator, bool newEnabled);
     event MarketCreated(IMarket indexed market, IMarket.MarketDefinition definition);
 
+    // sig: 0x0a37dc74
     error FactoryInvalidPayoffError();
+    // sig: 0x5116bce5
     error FactoryInvalidOracleError();
+    // sig: 0x213e2260
     error FactoryAlreadyRegisteredError();
 
+    // sig: 0x4dc1bc59
     error ProtocolParameterStorageInvalidError();
 
     function oracleFactory() external view returns (IFactory);
