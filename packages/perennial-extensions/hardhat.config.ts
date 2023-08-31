e176cccc37af7f1c950d8bb1fa424f4f1bf24e12
Arjun Rao <2940142+arjun-io@users.noreply.github.com>
2023-08-31 15:54:48 -0400

    upg root dep

--- packages/perennial-extensions/hardhat.config.ts
+++ packages/perennial-extensions/hardhat.config.ts
@@ -6,13 +6,13 @@ const config = defaultConfig({
     '@equilibria/perennial-v2-vault/contracts/Vault.sol',
     '@equilibria/perennial-v2-vault/contracts/VaultFactory.sol',
     '@equilibria/perennial-v2-oracle/contracts/interfaces/IPythOracle.sol',
-    '@equilibria/perennial-v2-oracle/contracts/pyth/PythOracle.sol',
+    '@equilibria/perennial-v2-oracle/contracts/pyth/PythOracle/PythOracle_Arbitrum.sol',
     '@equilibria/perennial-v2-oracle/contracts/pyth/PythFactory.sol',
     '@equilibria/perennial-v2-oracle/contracts/Oracle.sol',
     '@equilibria/perennial-v2-oracle/contracts/OracleFactory.sol',
     '@equilibria/perennial-v2-payoff/contracts/payoff/PowerTwo.sol',
     '@equilibria/perennial-v2-payoff/contracts/PayoffFactory.sol',
-    '@equilibria/root/attribute/Kept.sol',
+    '@equilibria/root/attribute/Kept/Kept.sol',
   ],
 })
 
