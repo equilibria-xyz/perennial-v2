0315e88c844e4c4f0d4b7df449876cdf3122d08f
nd <79500129+test9955667@users.noreply.github.com>
2023-08-31 16:12:34 -0400

    remove isntance test comment

--- packages/perennial-extensions/test/unit/MultiInvoker/MultiInvoker.test.ts
+++ packages/perennial-extensions/test/unit/MultiInvoker/MultiInvoker.test.ts
@@ -376,25 +376,6 @@ describe('MultiInvoker', () => {
     })
 
     describe('#reverts on', async () => {
-      // TODO instance modfiier in other pr
-      // it('reverts update, vaultUpdate, placeOrder on InvalidInstanceError', async () => {
-      //   await expect(
-      //     multiInvoker.connect(user).invoke(helpers.buildUpdateMarket({ market: vault.address })),
-      //   ).to.be.revertedWithCustomError(multiInvoker, 'MultiInvokerInvalidInstanceError')
-
-      //   await expect(
-      //     multiInvoker.connect(user).invoke(helpers.buildUpdateVault({ vault: market.address })),
-      //   ).to.be.revertedWithCustomError(multiInvoker, 'MultiInvokerInvalidInstanceError')
-
-      //   const trigger = openTriggerOrder({ size: collateral, price: 1100e6 })
-
-      //   await expect(
-      //     multiInvoker
-      //       .connect(user)
-      //       .invoke(buildPlaceOrder({ market: vault.address, collateral: collateral, order: trigger })),
-      //   ).to.be.revertedWithCustomError(multiInvoker, 'MultiInvokerInvalidInstanceError')
-      // })
-
       it('reverts placeOrder on InvalidOrderError', async () => {
         // Case 0 fee
         let trigger = openTriggerOrder({ size: position, price: BigNumber.from(1100e6), feePct: 0 })
