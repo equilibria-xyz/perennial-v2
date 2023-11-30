0788e24ee94fa491f6d387ce13ca252ef64d82b6
Arjun Rao <2940142+arjun-io@users.noreply.github.com>
2023-11-30 11:54:20 -0500

    Change commit price to support bulk priceIds

--- packages/perennial-deploy/tasks/commitPrice.ts
+++ packages/perennial-deploy/tasks/commitPrice.ts
@@ -2,42 +2,68 @@ import '@nomiclabs/hardhat-ethers'
 import { task, types } from 'hardhat/config'
 import { HardhatRuntimeEnvironment, TaskArguments } from 'hardhat/types'
 import { EvmPriceServiceConnection } from '@pythnetwork/pyth-evm-js'
+import { utils } from 'ethers'
 
 const PYTH_ENDPOINT = 'https://hermes.pyth.network'
 
-export default task('commit-price', 'Commits a price for the given price id')
-  .addParam('priceid', 'The price id to commit', '', types.string)
-  .setAction(async ({ priceid: priceId }: TaskArguments, HRE: HardhatRuntimeEnvironment) => {
-    if (!priceId) throw new Error('No Price ID provided')
+export default task('commit-price', 'Commits a price for the given price ids')
+  .addParam('priceids', 'The price ids to commit (comma separated)', '', types.string)
+  .addFlag('dry', 'Do not commit prices, print out calldata instead')
+  .setAction(async ({ priceids: priceIds_, dry }: TaskArguments, HRE: HardhatRuntimeEnvironment) => {
+    if (!priceIds_) throw new Error('No Price ID provided')
+    const priceIds = priceIds_.split(',')
+    if (!priceIds.length) throw new Error('No Price ID provided')
+
     const {
       ethers,
       deployments: { get },
     } = HRE
 
-    const oracleFactory = await ethers.getContractAt('IOracleFactory', (await get('OracleFactory')).address)
-    const oracle = await ethers.getContractAt('Oracle', await oracleFactory.oracles(priceId))
-    const oracleGlobal = await oracle.callStatic.global()
-    const pythProvider = await ethers.getContractAt(
-      'IPythOracle',
-      (
-        await oracle.callStatic.oracles(oracleGlobal.current)
-      ).provider,
-    )
+    const commitments: { action: number; args: string }[] = []
 
-    const pyth = new EvmPriceServiceConnection(PYTH_ENDPOINT, { priceFeedRequestConfig: { binary: true } })
-    const [minValidTime, versionListLength] = await Promise.all([
-      pythProvider.callStatic.MIN_VALID_TIME_AFTER_VERSION(),
-      pythProvider.callStatic.versionListLength(),
-    ])
+    for (const priceId of priceIds) {
+      const oracleFactory = await ethers.getContractAt('IOracleFactory', (await get('OracleFactory')).address)
+      const oracle = await ethers.getContractAt('Oracle', await oracleFactory.oracles(priceId))
+      const oracleGlobal = await oracle.callStatic.global()
+      const pythProvider = await ethers.getContractAt(
+        'IPythOracle',
+        (
+          await oracle.callStatic.oracles(oracleGlobal.current)
+        ).provider,
+      )
 
-    const vaa = await getRecentVaa({
-      pyth,
-      feedIds: [{ providerId: priceId, minValidTime: minValidTime.toBigInt() }],
-    })
+      const pyth = new EvmPriceServiceConnection(PYTH_ENDPOINT, { priceFeedRequestConfig: { binary: true } })
+      const [minValidTime, versionListLength] = await Promise.all([
+        pythProvider.callStatic.MIN_VALID_TIME_AFTER_VERSION(),
+        pythProvider.callStatic.versionListLength(),
+      ])
 
-    console.log('Committing VAA')
-    const { hash } = await pythProvider.commit(versionListLength, vaa[0].version, vaa[0].vaa, { value: 1n })
-    console.log('VAA committed. Hash:', hash)
+      const vaa = await getRecentVaa({
+        pyth,
+        feedIds: [{ providerId: priceId, minValidTime: minValidTime.toBigInt() }],
+      })
+
+      commitments.push(
+        buildCommitPrice({
+          ...vaa[0],
+          oracle: pythProvider.address,
+          value: 1n,
+          index: versionListLength.toBigInt(),
+          revertOnFailure: false,
+        }),
+      )
+    }
+
+    const multiInvoker = await ethers.getContractAt('IMultiInvoker', (await get('MultiInvoker')).address)
+
+    if (dry) {
+      console.log('Dry run, not committing. Calldata')
+      console.log(multiInvoker.interface.encodeFunctionData('invoke', [commitments]))
+    } else {
+      console.log('Committing VAAs')
+      const { hash } = await multiInvoker.invoke(commitments, { value: commitments.length })
+      console.log('VAA committed. Hash:', hash)
+    }
   })
 
 const getRecentVaa = async ({
@@ -65,3 +91,25 @@ const getRecentVaa = async ({
     }
   })
 }
+
+const buildCommitPrice = ({
+  oracle,
+  version,
+  value,
+  index,
+  vaa,
+  revertOnFailure,
+}: {
+  oracle: string
+  version: bigint
+  value: bigint
+  index: bigint
+  vaa: string
+  revertOnFailure: boolean
+}): { action: number; args: string } => ({
+  action: 6,
+  args: utils.defaultAbiCoder.encode(
+    ['address', 'uint256', 'uint256', 'uint256', 'bytes', 'bool'],
+    [oracle, value, index, version, vaa, revertOnFailure],
+  ),
+})
