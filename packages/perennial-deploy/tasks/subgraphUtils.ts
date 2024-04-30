42c1bd6a4151e0ec7c637f1ee891dcfd39cf86a2
Arjun Rao <2940142+arjun-io@users.noreply.github.com>
2024-04-30 12:51:51 -0400

    update env

--- packages/perennial-deploy/tasks/subgraphUtils.ts
+++ packages/perennial-deploy/tasks/subgraphUtils.ts
@@ -1,12 +1,14 @@
 // retrieve (private) subgraph URL used to query data for the desired chain
-export function getSubgraphUrlFromEnvironment(networkName: string) {
+export function getSubgraphUrlFromEnvironment(networkName: string): string {
   switch (networkName) {
     case 'arbitrum':
-      return process.env.ARBITRUM_GRAPH_URL
+      return process.env.ARBITRUM_GRAPH_URL ?? ''
     case 'arbitrumSepolia':
-      return process.env.ARBITRUMSEPOLIA_GRAPH_URL
+      return process.env.ARBITRUM_SEPOLIA_GRAPH_URL ?? ''
     case 'localhost':
       // caller must have FORK_NETWORK set for this to avoid fallback to default chain
       return getSubgraphUrlFromEnvironment(process.env.FORK_NETWORK ?? 'arbitrum')
   }
+
+  throw new Error(`Unsupported network: ${networkName}`)
 }
