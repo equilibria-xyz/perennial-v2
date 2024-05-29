259f27323682cc240e6a4d535d6cd798bc2e8cc7
Ed Noepel <ed@noepel.net>
2024-05-29 15:35:22 -0400

    storage for RebalanceConfig

--- packages/perennial-account/contracts/types/RebalanceConfig.sol
+++ packages/perennial-account/contracts/types/RebalanceConfig.sol
@@ -4,20 +4,56 @@ pragma solidity ^0.8.13;
 import { UFixed6 } from "@equilibria/root/number/types/UFixed6.sol";
 import { Action, ActionLib } from "./Action.sol";
 
-/// TODO: these don't need all 256 bits; should fit within a single storage slot
+/// @dev Rebalancing configuration for a single market
 struct RebalanceConfig {
     /// @dev Percentage of collateral from the group to deposit into the market
     UFixed6 target;
     /// @dev Percentage away from the target at which keepers may rebalance
     UFixed6 threshold;
 }
+struct RebalanceConfigStorage { uint256 slot0; }
+using RebalanceConfigLib for RebalanceConfigStorage global;
 
+/// @title RebalanceConfigLib
+/// @notice Library used to hash and manage storage for rebalancing configuration for a single market
+library RebalanceConfigLib {
+    UFixed6 private constant MAX_PERCENT = UFixed6.wrap(1e6); // 100%
+
+    /// sig: 0xd673935e
+    error RebalanceConfigStorageInvalidError();
+
+    /// @dev extracts two unsigned values from a single storage slot
+    function read(RebalanceConfigStorage storage self) internal view returns (RebalanceConfig memory) {
+        uint256 slot0 = self.slot0;
+        return RebalanceConfig(
+            UFixed6.wrap(uint256(slot0 << (256 - 128)) >> (256 - 128)),
+            UFixed6.wrap(uint256(slot0 << (256 - 128 - 128)) >> (256 - 128))
+        );
+    }
+
+    /// @dev ensures values do not exceed 100% and writes them to a single storage slot
+    function store(RebalanceConfigStorage storage self, RebalanceConfig memory newValue) external {
+        if (newValue.target.gt(MAX_PERCENT)) revert RebalanceConfigStorageInvalidError();
+        if (newValue.threshold.gt(MAX_PERCENT)) revert RebalanceConfigStorageInvalidError();
+
+        uint256 encoded0 =
+            uint256(UFixed6.unwrap(newValue.target)    << (256 - 128)) >> (256 - 128) |
+            uint256(UFixed6.unwrap(newValue.threshold) << (256 - 128)) >> (256 - 128 - 128);
+
+        assembly {
+            sstore(self.slot, encoded0)
+        }
+    }
+}
+
+/// @dev Action message to change configuration for a group of markets
 struct RebalanceConfigChange {
     /// @dev Identifies the group to update; set to 0 to create a new group
     uint256 group;
     /// @dev Total collateral to distribute across markets in the group
     UFixed6 totalCollateral;
-    /// @dev List of markets in which collateral shall be managed
+    /// @dev List of 1-8 markets in which collateral shall be managed.
+    /// Markets may be added to or removed from an existing group. Leave empty to delete the group.
     address[] markets;
     /// @dev Target allocation for markets in the aforementioned array
     RebalanceConfig[] configs;
