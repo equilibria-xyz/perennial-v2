5d5c594ce7f4b5901847921622b68de690177b41
Ed Noepel <ed@noepel.net>
2024-06-18 13:20:44 -0400

    tidy

--- packages/perennial-account/contracts/types/DeployAccount.sol
+++ packages/perennial-account/contracts/types/DeployAccount.sol
@@ -12,14 +12,14 @@ using DeployAccountLib for DeployAccount global;
 /// @title DeployAccountLib
 /// @notice Library used to hash and verify action to deploy a collateral account
 library DeployAccountLib {
-    /// @dev used to verify a signed message
+    /// @dev Used to verify a signed message
     bytes32 constant public STRUCT_HASH = keccak256(
         "DeployAccount(Action action)"
         "Action(uint256 maxFee,Common common)"
         "Common(address account,address signer,address domain,uint256 nonce,uint256 group,uint256 expiry)"
     );
 
-    /// @dev used to create a signed message
+    /// @dev Used to create a signed message
     function hash(DeployAccount memory self) internal pure returns (bytes32) {
         return keccak256(abi.encode(STRUCT_HASH, ActionLib.hash(self.action)));
     }
