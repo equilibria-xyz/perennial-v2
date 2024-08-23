dc17bceefc4e849cf7ed7f2438efc26c913704c2
Kevin Britz <kevin@equilibria.xyz>
2024-08-23 02:07:06 -0700

    add migration

--- packages/perennial/contracts/interfaces/IMarket.sol
+++ packages/perennial/contracts/interfaces/IMarket.sol
@@ -135,6 +135,7 @@ interface IMarket is IInstance {
     error VersionStorageInvalidError();
 
     function initialize(MarketDefinition calldata definition_) external;
+    function migrate() external;
     function token() external view returns (Token18);
     function oracle() external view returns (IOracleProvider);
     function beneficiary() external view returns (address);
