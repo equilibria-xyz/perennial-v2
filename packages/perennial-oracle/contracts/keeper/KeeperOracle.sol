8a4dd0cb5f4e8593617af5351c963bc59bcd6b43
Kevin Britz <kevin@equilibria.xyz>
2024-09-09 11:30:30 -0700

    add try/catch fix

--- packages/perennial-oracle/contracts/keeper/KeeperOracle.sol
+++ packages/perennial-oracle/contracts/keeper/KeeperOracle.sol
@@ -144,12 +144,13 @@ contract KeeperOracle is IKeeperOracle, Instance {
 
         emit OracleProviderVersionFulfilled(version);
 
-        IMarket market = oracle.market();
-        if (market == IMarket(address(0))) return; // v2.3 migration -- don't callback if market is not yet set
-
-        market.settle(address(0));
-        oracle.claimFee(priceResponse.toOracleReceipt(_localCallbacks[version.timestamp].length()).settlementFee);
-        market.token().push(receiver, UFixed18Lib.from(priceResponse.syncFee));
+        try oracle.market() returns (IMarket market) { // v2.3 migration -- don't callback if Oracle is still on its v2.2 implementation
+            market.settle(address(0));
+            oracle.claimFee(priceResponse.toOracleReceipt(_localCallbacks[version.timestamp].length()).settlementFee);
+            market.token().push(receiver, UFixed18Lib.from(priceResponse.syncFee));
+        } catch {
+            return;
+        }
     }
 
     /// @notice Performs an asynchronous local settlement callback
