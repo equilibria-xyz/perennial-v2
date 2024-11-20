b2ba3c309f5355c2e6db18136744e441467b1c61
Prateek <prateek@perennial.finance>
2024-11-20 19:13:42 +0530

    add inheritdoc and check market if created from same market factory

--- packages/core/contracts/InsuranceFund.sol
+++ packages/core/contracts/InsuranceFund.sol
@@ -8,42 +8,42 @@ import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 import { IInsuranceFund } from "./interfaces/IInsuranceFund.sol";
 import { IMarket } from "./interfaces/IMarket.sol";
+import { IMarketFactory } from "./interfaces/IMarketFactory.sol";
+import { IOracleProvider } from "./interfaces/IOracleProvider.sol";
 
 /// @title InsuranceFund
 /// @notice This contract manages the protocol fee and shortfalls for markets.
 contract InsuranceFund is IInsuranceFund, Ownable {
 
-    /// @notice The address of the market factory owner.
+    /// @dev The address of the market factory owner.
+    IMarketFactory public marketFactory;
+
+    /// @dev The address of the market factory owner.
     address public marketFactoryOwner;
 
-    /// @notice The address of DSU token
+    /// @dev The address of DSU token
     Token18 public DSU;
 
-    /**
-     * @notice Initializes the InsuranceFund contract.
-     * @param _marketFactoryOwner The address of the market factory owner.
-     * @param _token The address of the DSU token.
-     */
-    function initialize(address _marketFactoryOwner, Token18 _token) external initializer(1) {
+    /// @inheritdoc IInsuranceFund
+    function initialize(IMarketFactory _marketFactory, Token18 _token) external initializer(1) {
         __Ownable__initialize();
-        marketFactoryOwner = _marketFactoryOwner;
+        marketFactory = _marketFactory;
+        marketFactoryOwner = marketFactory.owner();
         DSU = _token;
     }
 
-    /**
-     * @notice Claims fees from a market.
-     * @param market The address of the market from which to claim protocol fees.
-     */
+    /// @inheritdoc IInsuranceFund
     function claimFees(address market) external {
         if (market == address(0)) revert InsuranceFundInvalidAddress();
+
+        // Check market is created from market factory
+        IOracleProvider oracle = IMarket(market).oracle();
+        if (market != address(marketFactory.markets(oracle))) revert InsuranceFundInvalidAddress();
+
         IMarket(market).claimFee(marketFactoryOwner);
     }
 
-    /**
-     * @notice Resolves any shortfall in a market.
-     * @param market The address of the market for which to resolve the shortfall.
-     * @dev This function can only be called by the owner.
-     */
+    /// @inheritdoc IInsuranceFund
     function resolveShortfall(address market) external onlyOwner {
         if (market == address(0)) revert InsuranceFundInvalidAddress();
 
@@ -53,21 +53,14 @@ contract InsuranceFund is IInsuranceFund, Ownable {
         IMarket(market).claimExposure();
     }
 
-    /**
-     * @notice Sends DSU tokens to a market contract.
-     * @param market The address of the market contract to send DSU tokens to.
-     * @param amount The amount of DSU tokens to send.
-     */
+    /// @inheritdoc IInsuranceFund
     function sendDSUToMarket(address market, UFixed18 amount) external onlyOwner {
         if (market == address(0)) revert InsuranceFundInvalidAddress();
         if (amount.isZero()) revert InsuranceFundInvalidAmount();
         DSU.push(market, amount);
     }
 
-    /**
-     * @notice Withdraws DSU tokens from the contract.
-     * @param amount The amount of DSU tokens to withdraw.
-     */
+    /// @inheritdoc IInsuranceFund
     function withdrawDSU(UFixed18 amount) external onlyOwner {
         if (amount.isZero() || DSU.balanceOf(address(this)).lt(amount)) revert InsuranceFundInvalidAmount();
         DSU.push(msg.sender, amount);
--- packages/core/contracts/interfaces/IInsuranceFund.sol
+++ packages/core/contracts/interfaces/IInsuranceFund.sol
@@ -5,13 +5,36 @@ import { IOwnable } from "@equilibria/root/attribute/interfaces/IOwnable.sol";
 import { Token18 } from "@equilibria/root/token/types/Token18.sol";
 import { UFixed18 } from "@equilibria/root/number/types/UFixed18.sol";
 
+import { IMarketFactory } from "./IMarketFactory.sol";
+
+/// @notice This contract manages the protocol fee and shortfalls for markets.
 interface IInsuranceFund is IOwnable {
+     
+    /// @custom:error Thrown when an invalid address is provided.
     error InsuranceFundInvalidAddress();
+
+    /// @custom:error Thrown when an invalid amount is provided.
     error InsuranceFundInvalidAmount();
 
-    function initialize(address, Token18) external;
-    function claimFees(address) external;
-    function resolveShortfall(address) external;
-    function sendDSUToMarket(address, UFixed18) external;
-    function withdrawDSU(UFixed18) external;
+    /// @notice Initializes the InsuranceFund contract.
+    /// @param _marketFactory The address of the market factory.
+    /// @param _token The address of the DSU token.
+    function initialize(IMarketFactory _marketFactory, Token18 _token) external;
+
+    /// @notice Claims fees from a market.
+    /// @param market The address of the market from which to claim protocol fees.
+    function claimFees(address market) external;
+
+    /// @notice Resolves any shortfall in a market.
+    /// @param market The address of the market for which to resolve the shortfall.
+    function resolveShortfall(address market) external;
+
+    /// @notice Sends DSU tokens to a market contract.
+    /// @param market The address of the market contract to send DSU tokens to.
+    /// @param amount The amount of DSU tokens to send.
+    function sendDSUToMarket(address market, UFixed18 amount) external;
+
+    /// @notice Withdraws DSU tokens from the contract.
+    /// @param amount The amount of DSU tokens to withdraw.
+    function withdrawDSU(UFixed18 amount) external;
 }
