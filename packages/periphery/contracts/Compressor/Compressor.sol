cae58fc3fc7cf20e9447bdb95cb2aef28888f4b5
Kevin Britz <kevin@perennial.finance>
2025-01-18 02:04:36 -0800

    update with market order payload

--- packages/periphery/contracts/Compressor/Compressor.sol
+++ packages/periphery/contracts/Compressor/Compressor.sol
@@ -5,11 +5,14 @@ import { UFixed18 } from "@equilibria/root/number/types/UFixed18.sol";
 import { Fixed6, Fixed6Lib } from "@equilibria/root/number/types/Fixed6.sol";
 import { UFixed6 } from "@equilibria/root/number/types/UFixed6.sol";
 import { Token18 } from "@equilibria/root/token/types/Token18.sol";
+import { IOracleFactory } from "@perennial/v2-oracle/contracts/interfaces/IOracleFactory.sol";
 import { IPythFactory } from "@perennial/v2-oracle/contracts/interfaces/IPythFactory.sol";
 import { IMarket } from "@perennial/v2-core/contracts/interfaces/IMarket.sol";
+import { Take } from "@perennial/v2-core/contracts/types/Take.sol";
 import { IMultiInvoker } from "../MultiInvoker/interfaces/IMultiInvoker.sol";
-import { IController } from "../CollateralAccounts/interfaces/IController.sol";
+import { Controller_Incentivized } from "../CollateralAccounts/Controller_Incentivized.sol";
 import { MarketTransfer } from "../CollateralAccounts/types/MarketTransfer.sol";
+import { RelayedTake } from "../CollateralAccounts/types/RelayedTake.sol";
 import { Action as CAAction } from "../CollateralAccounts/types/Action.sol";
 import { Common, CommonLib } from "@equilibria/root/verifier/types/Common.sol";
 import { IManager } from "../TriggerOrders/interfaces/IManager.sol";
@@ -19,54 +22,91 @@ import { InterfaceFee } from "../TriggerOrders/types/InterfaceFee.sol";
 import { Action as TOAction} from "../TriggerOrders/types/Action.sol";
 
 /// @title Compressor
-/// @notice Manages claiming fees and updating risk parameters for markets
+/// @notice Compresses the calldata of standard relayer message bundles to reduce calldata size.
 contract Compressor {
     // constants
-    uint256 public immutable PYTH_COMMIT_VALUE = 1;
+    uint256 public constant PYTH_COMMIT_VALUE = 1;
+    uint256 public constant EXPIRY_PERIOD = 2 minutes;
 
     // protocol contracts
     Token18 public immutable dsu;
     IMultiInvoker public immutable multiInvoker;
     IPythFactory public immutable pythFactory;
-    IController public immutable controller;
+    Controller_Incentivized public immutable controller;
     IManager public immutable manager;
+    IOracleFactory public immutable oracleFactory;
 
     // compressor settings
     address public immutable referrer;
 
-    struct InvokeParams {
+    constructor(
+        Token18 dsu_,
+        IMultiInvoker multiInvoker_,
+        IPythFactory pythFactory_,
+        Controller_Incentivized controller_,
+        IManager manager_,
+        IOracleFactory oracleFactory_,
+        address referrer_
+    ) {
+        dsu = dsu_;
+        multiInvoker = multiInvoker_;
+        pythFactory = pythFactory_;
+        controller = controller_;
+        manager = manager_;
+        oracleFactory = oracleFactory_;
+        referrer = referrer_;
+    }
+
+    /*
+        Encoding instructions
+          - Must use the specified immutable `referrer` in all applicable fields
+          - Each message should use the same `group`
+          - Each message should use and `expiry` value of `version + EXPIRY_PERIOD`
+          - Nonces must be incrementing form supplied nonce
+            - marketTransferNonce (nonce)
+            - marketOrderInnerNonce (nonce + 1)
+            - marketOrderOuterNonce (nonce + 2)
+            - triggerOrderSLNonce (nonce + 3)
+            - triggerOrderTPNonce (nonce + 4)
+          - Trigger orders must be full closes of the market order (amount * -1)
+          - Trigger orders must use the same interface fee amount
+          - Collateral account actions must use the same collateral account fee
+          - `priceFeedId` is inferred from the `market`
+    */
+
+    struct OrderBundleParams {
         bytes priceCommitmentData;
-        bytes32 priceFeedId; // this could be looked up from market
-        uint256 version; // could tyie expiry to version
+        uint256 version;
 
         IMarket market;
         address account;
         address signer;
+
         Fixed6 tradeCollateral;
         Fixed6 tradeAmount;
         Fixed6 minPrice;
         Fixed6 maxPrice;
 
+        uint256 group;
+        uint256 nonce;
         UFixed6 relayerMaxFee;
         UFixed6 triggerOrderMaxFee;
-        uint256 group;
-        uint256 expiry;
+
+        UFixed6 triggerOrderInterfaceFee;
+        uint256 triggerOrderSLId;
+        uint256 triggerOrderTPId;
+
         bytes marketTransferSignature;
+        bytes marketOrderOuterSignature;
+        bytes marketOrderInnerSignature;
         bytes triggerOrderSLSignature;
         bytes triggerOrderTPSignature;
-
-        UFixed6 tradeFeeAmount; // should we calculate this deterministically? (trigger orders)
-        uint256 triggerOrderSLId; // Should this be nonce?
-        uint256 triggerOrderTPId; // Should this be nonce?
-        uint256 marketTransferNonce;
-        uint256 triggerOrderSLNonce;
-        uint256 triggerOrderTPNonce;
     }
 
-    function invoke(InvokeParams calldata p) external payable sweepDSU {
+    function placeOrderBundle(OrderBundleParams calldata p) external payable sweepDSU {
         // commit price
         bytes32[] memory ids = new bytes32[](1);
-        ids[0] = p.priceFeedId;
+        ids[0] = oracleFactory.ids(p.market.oracle());
         _commitPrice(PYTH_COMMIT_VALUE, ids, p.version, p.priceCommitmentData);
 
         // market transfer
@@ -80,9 +120,9 @@ contract Compressor {
                         p.account,
                         p.signer,
                         address(controller),
-                        p.marketTransferNonce,
+                        p.nonce,
                         p.group,
-                        p.expiry
+                        p.version + EXPIRY_PERIOD
                     )
                 )
             ),
@@ -90,6 +130,35 @@ contract Compressor {
         );
 
         // amm market order
+        controller.relayTake(
+            RelayedTake(
+                Take(
+                    p.tradeAmount,
+                    referrer,
+                    Common(
+                        p.account,
+                        p.signer,
+                        address(controller),
+                        p.nonce + 1,
+                        p.group,
+                        p.version + EXPIRY_PERIOD
+                    )
+                ),
+                CAAction(
+                    p.relayerMaxFee,
+                    Common(
+                        p.account,
+                        p.signer,
+                        address(controller),
+                        p.nonce + 2,
+                        p.group,
+                        p.version + EXPIRY_PERIOD
+                    )
+                )
+            ),
+            p.marketOrderOuterSignature,
+            p.marketOrderInnerSignature
+        );
 
         // trigger order (SL)
         manager.placeOrderWithSignature(
@@ -103,7 +172,7 @@ contract Compressor {
                     false,
                     referrer,
                     InterfaceFee(
-                        p.tradeFeeAmount,
+                        p.triggerOrderInterfaceFee,
                         referrer,
                         true,
                         false
@@ -117,9 +186,9 @@ contract Compressor {
                         p.account,
                         p.signer,
                         address(manager),
-                        p.triggerOrderSLNonce,
+                        p.nonce + 3,
                         p.group,
-                        p.expiry
+                        p.version + EXPIRY_PERIOD
                     )
                 )
             ),
@@ -138,7 +207,7 @@ contract Compressor {
                     false,
                     referrer,
                     InterfaceFee(
-                        p.tradeFeeAmount,
+                        p.triggerOrderInterfaceFee,
                         referrer,
                         true,
                         false
@@ -152,9 +221,9 @@ contract Compressor {
                         p.account,
                         p.signer,
                         address(manager),
-                        p.triggerOrderTPNonce,
+                        p.nonce + 5,
                         p.group,
-                        p.expiry
+                        p.version + EXPIRY_PERIOD
                     )
                 )
             ),
