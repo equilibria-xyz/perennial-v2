d993e41606bd1807d84c98d364cb30c6147d48c9
Ed Noepel <ed@noepel.net>
2025-01-22 12:51:58 -0500

    removed unintentional chicken

--- packages/periphery/pe2105.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-yarn test --grep 'Manager' &&
-yarn test:integrationArbitrum --grep 'Manager_Arbitrum' &&
-yarn test:integrationBase --grep 'Manager_Optimism'
--- packages/periphery/pe2122.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-yarn test:integrationArbitrum --grep 'relays take messages' &&
-yarn test --grep 'Controller_Incentivized' &&
-yarn test --grep '#relayed'
