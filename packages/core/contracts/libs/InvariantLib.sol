347da6630dabaac3f6ac4d5e55f6b1810e6332d4
Prateek <prateek@perennial.finance>
2025-03-13 17:25:26 +0530

    update margin check to apply only if order is increasing position or collateral

--- packages/core/contracts/libs/InvariantLib.sol
+++ packages/core/contracts/libs/InvariantLib.sol
@@ -86,7 +86,7 @@ library InvariantLib {
         ) revert IMarket.MarketExceedsPendingIdLimitError();
 
         if (
-            (newOrder.increasesPosition() || !newOrder.collateral.eq(Fixed6Lib.ZERO)) &&   // the order is increasing position or modifying collateral
+            (newOrder.increasesPosition() || newOrder.collateral.gt(Fixed6Lib.ZERO)) &&   // the order is increasing position or collateral
             !PositionLib.margined(
                 _worstCasePendingLocal(context, updateContext),
                 context.latestOracleVersion,
