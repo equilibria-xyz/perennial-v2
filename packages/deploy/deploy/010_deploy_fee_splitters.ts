f5abc313c8414a4e36ede122788601fc26f43cff
Prateek <prateek@perennial.finance>
2025-05-30 16:29:36 +0530

    add check for airdrop contract address and registration of all market in fee coordinator

--- packages/deploy/deploy/010_deploy_fee_splitters.ts
+++ packages/deploy/deploy/010_deploy_fee_splitters.ts
@@ -3,11 +3,12 @@ import { HardhatRuntimeEnvironment } from 'hardhat/types'
 import { DeployFunction } from 'hardhat-deploy/types'
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
 import { FeeCoordinator__factory, ProxyAdmin__factory } from '../types/generated'
-import { getMultisigAddress } from '../../common/testutil/constants'
+import { getMultisigAddress, getAirdropAddress } from '../../common/testutil/constants'
+import { ChainMarkets } from '@perennial/sdk'
 
 const SkipIfAlreadyDeployed = false
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  const { deployments, getNamedAccounts, ethers } = hre
+  const { deployments, getNamedAccounts, getChainId, ethers } = hre
   const { deploy, get, getNetworkName } = deployments
   const { deployer } = await getNamedAccounts()
   const protocolMultisig = getMultisigAddress(getNetworkName())
@@ -52,34 +53,45 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   })
   const feeCoordinator = new FeeCoordinator__factory(deployerSigner).attach((await get('FeeCoordinator')).address)
 
-  const airdropContractAddress = (await get('Airdrop')).address
-  console.log('Deploying fee splitters...')
-  // deploy fee splitters
-  console.log('Deploying PerpFun fee splitter...')
-  const perpFunFeeSplitter = await feeCoordinator.callStatic.create(airdropContractAddress)
-  await feeCoordinator.create(airdropContractAddress)
-  console.log('PerpFun fee splitter address:', perpFunFeeSplitter)
+  const airdropContractAddress = getAirdropAddress(getNetworkName())
 
-  console.log('Deploying Pro App fee splitter...')
-  const proAppFeeSplitter = await feeCoordinator.callStatic.create(airdropContractAddress)
-  await feeCoordinator.create(airdropContractAddress)
-  console.log('Pro App fee splitter address:', proAppFeeSplitter)
+  if (airdropContractAddress) {
+    console.log('Deploying fee splitters...')
+    // deploy fee splitters
+    console.log('Deploying PerpFun fee splitter...')
+    const perpFunFeeSplitter = await feeCoordinator.callStatic.create(airdropContractAddress)
+    await feeCoordinator.create(airdropContractAddress)
+    console.log('PerpFun fee splitter address:', perpFunFeeSplitter)
 
-  console.log('Deploying Autopilot fee splitter...')
-  const autopilotFeeSplitter = await feeCoordinator.callStatic.create(airdropContractAddress)
-  await feeCoordinator.create(airdropContractAddress)
-  console.log('Autopilot fee splitter address:', autopilotFeeSplitter)
+    console.log('Deploying Pro App fee splitter...')
+    const proAppFeeSplitter = await feeCoordinator.callStatic.create(airdropContractAddress)
+    await feeCoordinator.create(airdropContractAddress)
+    console.log('Pro App fee splitter address:', proAppFeeSplitter)
 
-  console.log('Deploying Liquidator fee splitter...')
-  const liquidatorFeeSplitter = await feeCoordinator.callStatic.create(airdropContractAddress)
-  await feeCoordinator.create(airdropContractAddress)
-  console.log('Liquidator fee splitter address:', liquidatorFeeSplitter)
-  console.log('Fee splitters deployed successfully')
+    console.log('Deploying Autopilot fee splitter...')
+    const autopilotFeeSplitter = await feeCoordinator.callStatic.create(airdropContractAddress)
+    await feeCoordinator.create(airdropContractAddress)
+    console.log('Autopilot fee splitter address:', autopilotFeeSplitter)
+
+    console.log('Deploying Liquidator fee splitter...')
+    const liquidatorFeeSplitter = await feeCoordinator.callStatic.create(airdropContractAddress)
+    await feeCoordinator.create(airdropContractAddress)
+    console.log('Liquidator fee splitter address:', liquidatorFeeSplitter)
+    console.log('Fee splitters deployed successfully')
+  }
 
   if ((await feeCoordinator.pendingOwner()) === constants.AddressZero && !!protocolMultisig) {
     process.stdout.write('Updating fee coordinator pending owner...')
     await feeCoordinator.updatePendingOwner(protocolMultisig)
     process.stdout.write('complete\n')
+  }
+
+  const chainId = await getChainId()
+  const markets = ChainMarkets[Number(chainId) as keyof typeof ChainMarkets]
+  for (const market of Object.entries(markets)) {
+    console.log(`Registering market ${market[1]} with FeeCoordinator...`)
+    await feeCoordinator.register(market[1])
+    console.log(`Market ${market[1]} registered.`)
   }
 }
 
